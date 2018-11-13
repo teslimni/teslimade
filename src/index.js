918a3e9feac6735170b42da550ffac8c647381d1
Teslim Adeyemo <teslimni@gmail.com>
2018-11-13 03:22:57 +0100

    Initialised webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './main.css';
+var name = "Teslim Olanrewaju Abiodun Adeyemo";
+
+class Form {
+    constructor() {
+        let numbers = [15, 20, 25].map(number => number *2 );
+
+        console.log(numbers);
+    }
+}
+
+new Form();
